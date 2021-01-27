698dc5642a11bd44435bcf8f8b9571d6570bda2f
Rafael Silva e Silva <rafaelssilva35@gmail.com>
2021-01-26 21:02:31 -0300

    migrations

---
+++ database/migrations/2021_01_26_233621_create_endereco.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEndereco extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('endereco', function (Blueprint $table) {
+            $table->id();
+            $table->string('logradouro');
+            $table->string('complemento');
+            $table->string('bairro');
+            $table->string('localidade');            
+            $table->unsignedBigInteger('uf_id');
+            $table->foreign('uf_id')->references('id')->on('uf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('endereco');
+    }
+}
